c6d3b20955a4e1bb3339885aac8522b604a517d4
Tim McMackin <timothymcmackin@yahoo.com>
2024-01-11 08:17:46 -0500

    Fit border to content and allow width setting

--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -2,10 +2,10 @@
 import React from 'react'
 import useBaseUrl from '@docusaurus/useBaseUrl'
 
-export default function Figure({ src, caption }) {
+export default function Figure({ src, caption, width }) {
   return (
-    <figure style={{ border: '1px solid #888', padding: 20 }}>
-      <img src={useBaseUrl(src)} alt={caption} />
+    <figure style={{ border: '1px solid #888', padding: 20, width: 'fit-content' }}>
+      <img src={useBaseUrl(src)} alt={caption} width={width}/>
       <figcaption>{caption}</figcaption>
     </figure>
   )
