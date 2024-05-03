f5cad838bf22ee3655fa0fb8a24c2d55eaaf8629
Rohan Tangri <rohan.tangri@trili.tech>
2024-05-03 10:52:36 +0100

    default dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'light',
+        defaultMode: "dark",
         disableSwitch: false,
         respectPrefersColorScheme: true,
       },
