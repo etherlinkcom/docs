01936a9ac217f1edb114c3b090295c7aa9b80917
Tim McMackin <timothymcmackin@yahoo.com>
2024-07-08 12:59:07 -0400

    Use Docusaurus copy button and icons

--- src/components/InlineCopy/index.jsx
+++ src/components/InlineCopy/index.jsx
@@ -3,16 +3,13 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-export default function InlineCopy({ href }) {
+import CopyButton from '@site/src/components/CopyButton'
+
+export default function InlineCopy({ code }) {
   return (
     <div className={clsx(styles.container)}>
-      <code>{href}</code>
-      <button
-        className={clsx(styles.copy_button)} type="button" aria-label="Copy to clipboard"
-        onClick={() => { navigator.clipboard.writeText(href) }}
-      >
-        <svg width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"></path></svg>
-      </button>
+      <code>{code}</code>
+      <CopyButton className={clsx(styles.codeButton)} code={code} />
     </div>
   );
 }
