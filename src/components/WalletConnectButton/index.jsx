50dd3a0d2b3f205cadcf3a2bbb9c7b2ef9706a74
Tim McMackin <timothymcmackin@yahoo.com>
2024-07-09 10:18:34 -0400

    Customize buttons for both networks

--- src/components/WalletConnectButton/index.jsx
+++ src/components/WalletConnectButton/index.jsx
@@ -11,25 +11,43 @@ const customTheme = lightTheme({
   },
 });
 
-export default function WalletConnectButtons() {
-  const activeChain = {
-    chainId: 128123,
-    rpc: ["https://node.ghostnet.etherlink.com"],
-    nativeCurrency: {
-      decimals: 18,
-      name: "XTZ",
-      symbol: "XTZ",
-    },
-    shortName: "etherlink",
-    slug: "etherlink",
-    testnet: true,
-    chain: "Etherlink",
-    name: "Etherlink Testnet",
-  };
+const ghostnet = {
+  chainId: 128123,
+  rpc: ["https://node.ghostnet.etherlink.com"],
+  nativeCurrency: {
+    decimals: 18,
+    name: "XTZ",
+    symbol: "XTZ",
+  },
+  shortName: "etherlink",
+  slug: "etherlink",
+  testnet: true,
+  chain: "Etherlink",
+  name: "Etherlink Testnet",
+};
+
+const mainnet = {
+  chainId: 42793,
+  rpc: ["https://node.mainnet.etherlink.com"],
+  nativeCurrency: {
+    decimals: 18,
+    name: "XTZ",
+    symbol: "XTZ",
+  },
+  shortName: "etherlink",
+  slug: "etherlink",
+  testnet: true,
+  chain: "Etherlink",
+  name: "Etherlink Mainnet",
+};
+
+export default function WalletConnectButton({ network, title }) {
+
+  const activeChain = network === "mainnet" ? mainnet : ghostnet;
 
   const dAppMeta = {
-    name: "Etherlink Testnet Faucet",
-    description: "Drip Testnet XTZ",
+    name: "Etherlink documentation",
+    description: "Connect your wallet to Etherlink",
     logoUrl: "https://etherlink.com/logo.png",
     url: "https://etherlink.com",
     isDarkMode: true,
@@ -54,7 +72,7 @@ export default function WalletConnectButtons() {
         switchToActiveChain={true}
         theme={customTheme}
         modalSize={"wide"}
-        // btnTitle="Connect Etherlink To Metamask"
+        btnTitle={title}
       />
     </ThirdwebProvider>
 
