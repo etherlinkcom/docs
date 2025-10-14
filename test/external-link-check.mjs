48ca553b85b321e7c588f93fe2cf75142fd58f9b
Tim McMackin <timothymcmackin@yahoo.com>
2025-10-14 12:20:46 -0400

    Process markdown to AST to get links

--- test/external-link-check.mjs
+++ test/external-link-check.mjs
@@ -6,11 +6,26 @@ import fs from 'fs';
 import { glob } from 'glob';
 import linkCheck from 'link-check';
 
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { mdxjs } from 'micromark-extension-mdxjs';
+import { mdxFromMarkdown } from 'mdast-util-mdx';
+import { visit } from 'unist-util-visit';
+
+// https://unifiedjs.com/learn/guide/using-unified/
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeStringify);
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const docsFolder = path.resolve(__dirname, '../docs');
+const rootFolder = path.resolve(__dirname, '..');
+const docsFolder = path.resolve(rootFolder, './docs');
 
-const linkRegex = /https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm;
-
+// Ignore URLs that match these regexes
 const regexesToIgnore = [
   /127.0.0.1/,
   /https:\/\/explorer\.etherlink\.com/,
@@ -18,69 +33,84 @@ const regexesToIgnore = [
   /https:\/\/shadownet\.explorer\.etherlink\.com/,
 ];
 
-// Given a file path, load the file and get all external links in it
-const getLinksInFile = async (filePath) => {
+// Convert file to AST, using the correct processors for MD and MDX files
+const getAst = async (filePath) => {
   const fileContents = await fs.promises.readFile(filePath, 'utf8');
-  const externalLinksInFile = [];
-  let matches;
-  while ((matches = linkRegex.exec(fileContents)) !== null) {
-    // Trim periods at the end of the URL because my regex-fu is insufficient
-    const trimmedMatch = matches[0].endsWith('.') ? matches[0].slice(0, -1) : matches[0];
-    // Check if it's in the ignore list
-    const addToList = regexesToIgnore.reduce((runningResult, oneRegex) => {
-      if (!runningResult || oneRegex.test(trimmedMatch)) {
+  if (path.extname(filePath) === '.mdx') {
+    return fromMarkdown(fileContents, {
+      extensions: [mdxjs()],
+      mdastExtensions: [mdxFromMarkdown()]
+    });
+  } else if (path.extname(filePath) === '.md'){
+    return processor.parse(fileContents);
+  }
+}
+
+// Test function to select AST nodes that are links
+const markdownTestFunction = (node) => node.type === 'link';
+
+// Walk AST to get links
+const getLinksInAst = (ast) => {
+  const linksInAst = [];
+  visit(ast, markdownTestFunction, (node) => {
+    let addToList = true;
+    // Filter local links
+    if (node.url.startsWith('/') || node.url.startsWith('#')) addToList = false;
+    // Filter by regexes
+    addToList = addToList && regexesToIgnore.reduce((runningResult, oneRegex) => {
+      if (!runningResult || oneRegex.test(node.url)) {
         return false;
       }
       return runningResult;
     }, true);
-    if (addToList) {
-      externalLinksInFile.push(trimmedMatch);
-    }
-  }
-  return externalLinksInFile;
-}
-
-// Get all external links in all files
-const getAllExternalLinks = async (basePath) => {
-  const allMdFilePaths = await glob(basePath + '/**/*.{md,mdx}');
-  const allLinksPromises = allMdFilePaths.map(getLinksInFile);
-  const allExternalLinks = await allLinksPromises.reduce(async (runningListPromise, oneListPromise) => {
-    const runningListOfLinks = await runningListPromise;
-    const oneList = await oneListPromise;
-    oneList.forEach(oneLink => {
-      if (!runningListOfLinks.includes(oneLink)) {
-        runningListOfLinks.push(oneLink);
-      }
-    });
-    return runningListOfLinks;
-  }, Promise.resolve([]));
-  return allExternalLinks;
+    if (addToList) linksInAst.push(node.url);
+  });
+  return linksInAst;
 }
 
 // Check a link with https://www.npmjs.com/package/link-check
 const checkLink = async (url) => new Promise((resolve, reject) => {
   linkCheck(url, (err, result) => {
     if (err) reject(err);
-    if (result.status !== 'alive') reject(result.status);
-    resolve(result.status);
+    if (result.status !== 'alive') reject(result.statusCode);
+    resolve(result.statusCode);
   });
 });
 
 const runTest = async () => {
-  const allLinks = await getAllExternalLinks(docsFolder);
-  let foundABrokenLink = false;
-  await Promise.all(allLinks.map((oneLink) =>
-    checkLink(oneLink)
-      // .then((status) => console.log(oneLink, '-', status))
-      .catch((err) => {
-        foundABrokenLink = true;
-        console.error(oneLink, '-', err);
-      })
-  ));
-  if (foundABrokenLink) {
+
+  const linksChecked = [];
+  const brokenLinks = [];
+  const allMdFilePaths = await glob(docsFolder + '/**/*.{md,mdx}');
+
+  await Promise.all(allMdFilePaths.map(async (oneFilePath) => {
+    const oneAst = await getAst(oneFilePath);
+    const linksInAst = getLinksInAst(oneAst);
+    await Promise.all(linksInAst.map(async (oneLink) => {
+      if (!linksChecked.includes(oneLink)) {
+        await checkLink(oneLink)
+          .then(() => linksChecked.push(oneLink))
+          .catch((errOrStatus) => {
+            brokenLinks.push({
+              path: path.relative(rootFolder, oneFilePath),
+              url: oneLink,
+              err: errOrStatus,
+            });
+            linksChecked.push(oneLink);
+          }
+          );
+      }
+    }));
+  }));
+
+  console.log('Successfully checked', linksChecked.length, 'links.\n');
+  if (brokenLinks.length > 0) {
     console.error('Found broken links.');
-  } else {
-    console.log('Successfully checked', allLinks.length, 'links.');
+    brokenLinks.forEach(({path, url, err}) => {
+      console.error(`Broken link in ${path}:\n  ${url} returned ${err}\n`);
+    });
+    console.error('\n\n', 'Found', brokenLinks.length, 'broken links');
+    process.exit(1);
   }
 }
 
