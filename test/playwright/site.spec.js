523b64992c1319fb60f5d75ccea70fbf935a93c3
Tim McMackin <tim.mcmackin@trili.tech>
2026-02-03 13:49:25 -0500

    Remove this flaky test

--- test/playwright/site.spec.js
+++ test/playwright/site.spec.js
@@ -15,28 +15,6 @@ test('navigation', async ({ page }) => {
   await expect(page.getByText('How is Etherlink governed?')).toBeVisible();
 });
 
-test('code copy buttons', async ({ page, context }) => {
-  await context.grantPermissions(['clipboard-read']);
-  await page.goto('https://docs.etherlink.com/get-started/network-information');
-
-  // Click copy button
-  const mainnetAddressButton = page.locator('xpath=//a[@href="https://tzkt.io/sr1Ghq66tYK9y3r8CC1Tf8i8m5nxh8nTvZEf"]/following-sibling::button');
-  await mainnetAddressButton.click();
-
-  // Check clipboard contents
-  const clipboardContent = await page.evaluate(() => navigator.clipboard.readText());
-  await expect(clipboardContent).toEqual('sr1Ghq66tYK9y3r8CC1Tf8i8m5nxh8nTvZEf');
-
-  // Try a different button
-  const rpcButton = page.locator('xpath=//code[text()="https://node.mainnet.etherlink.com"]/following-sibling::button');
-  await rpcButton.click();
-
-  // Check clipboard contents
-  const clipboardContent2 = await page.evaluate(() => navigator.clipboard.readText());
-  await expect(clipboardContent2).toEqual('https://node.mainnet.etherlink.com');
-
-});
-
 test('search', async ({ page }) => {
   await page.goto('https://docs.etherlink.com/');
   await page.getByText("Search").click()
